f263856f7241054a65b897b90bc40398fe61a5ff
hayabusabusa <hayabusabusaSY@gmail.com>
2021-01-02 20:38:24 +0900

    initial commit

---
+++ XcodeGenSample/XcodeGenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGenSampleApp.swift
+//  XcodeGenSample
+//
+//  Created by Shunya Yamada on 2021/01/02.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
