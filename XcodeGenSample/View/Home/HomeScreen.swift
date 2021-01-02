558a92a377cf22911c548b031abae702a9796da9
hayabusabusa <hayabusabusaSY@gmail.com>
2021-01-03 02:19:39 +0900

    ui: remove default ContentView

---
+++ XcodeGenSample/View/Home/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  XcodeGenSample
+//
+//  Created by Shunya Yamada on 2021/01/03.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
--- XcodeGenSample/XcodeGenSampleApp.swift
+++ XcodeGenSample/XcodeGenSampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct XcodeGenSampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeScreen()
         }
     }
 }
